cd4075b17a011d7e82703ccdfa3e8feb207ac422
Parnian <parnian@Parnians-MacBook-Pro.local>
2017-01-28 12:08:17 +0100

    create countries model

---
+++ db/migrate/20170128110539_create_countries.rb
@@ -0,0 +1,14 @@
+class CreateCountries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :countries do |t|
+      t.string :name
+      t.string :flag
+      t.string :capital
+      t.string :language
+      t.integer :area
+      t.integer :population
+
+      t.timestamps
+    end
+  end
+end
